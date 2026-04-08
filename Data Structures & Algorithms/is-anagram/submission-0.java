602dbe422a0f38a836ed962f1364d0283ece1125
Jagadeeswar reddy Vankala <66000353+jaggu554@users.noreply.github.com>
2026-04-08 17:43:10 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length()!=t.length()) return false;
+
+        int freqS[]=new int[26];
+        int freqT[]=new int[26];
+
+        for(int i=0;i<s.length();i++){
+            freqS[s.charAt(i)-'a']++;
+            freqT[t.charAt(i)-'a']++;
+        }
+
+        if(freqS.equals(freqT)) return true;
+
+        System.out.println(Arrays.toString(freqS));
+
+        if(Arrays.equals(freqS,freqT)) return true;
+
+        
+
+    return false;
+    }
+}
